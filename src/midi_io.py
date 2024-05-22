b9e84b897978d308406a4b02d006aa9e7540f798
hammershock <hammershock@163.com>
2024-05-22 20:03:12 +0800

    feat: add start and end symbol when loading midifile

--- src/midi_io.py
+++ src/midi_io.py
@@ -1,10 +1,9 @@
+import json
 import os
 from typing import List, Dict, Literal, Union
 
-from pretty_midi import PrettyMIDI, Instrument, Note
 from joblib import Memory
-from tqdm import tqdm
-
+from pretty_midi import PrettyMIDI, Instrument, Note
 
 memory = Memory('../.cache', verbose=0)
 
@@ -31,19 +30,29 @@ def _process_data(midi_data: List[dict]):
 
 
 @memory.cache
-def load_file(file_path: str) -> List[Dict[Literal['start', 'end', 'pitch', 'velocity', 'duration', 'interval'], Union[float, int, str]]]:
+def load_file(file_path: str, max_length=512) -> List[
+    Dict[Literal['start', 'end', 'pitch', 'velocity', 'duration', 'interval'], Union[float, int, str]]]:
+    """
+    load symbols from midi file and wrap the notes with start symbol and end symbol
+    :param file_path:
+    :param max_length:
+    :return:
+    """
     midi = PrettyMIDI(file_path)
 
-    midi_data = []
-    for note in midi.instruments[0].notes:
-        data = {
-            'start': note.start,
-            'end': note.end,  # end time ascends
-            'pitch': note.pitch,  # 0-127
-            'velocity': note.velocity,  # 0-127
-            'duration': note.end - note.start
-        }
-        midi_data.append(data)
+    notes = midi.instruments[0].notes[:max_length - 2]
+
+    # We should find a good way to mark the special tokens, be sure special tokens has separate embeddings
+    start_symbol = {'start': 0, 'end': 0, 'pitch': -1, 'velocity': 64, 'duration': 0}
+    end_symbol = {'start': notes[-1].end, 'end': notes[-1].end, 'pitch': -2, 'velocity': 64, 'duration': 0}
+
+    midi_data = [start_symbol] + [{
+        'start': note.start,
+        'end': note.end,  # end time ascends
+        'pitch': note.pitch,  # 0-127
+        'velocity': note.velocity,  # 0-127
+        'duration': note.end - note.start
+    } for note in notes] + [end_symbol]
 
     return _process_data(midi_data)
 
@@ -69,8 +78,18 @@ def save_file(midi_data: List[Dict[str, Union[float, int, str]]], file_path: str
     midi.write(file_path)
 
 
+def load_metadata(file_path: str) -> Dict[str, Union[float, int, str]]:
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+        meta_data = {filename: data['duration'][key] for key, filename in data['midi_filename'].items()}
+        return meta_data
+
+
 if __name__ == "__main__":
-    data_dir = "../data"
-    files = get_midi_files(data_dir)
-    for file in tqdm(files):
-        midi_data = load_file(file)
+    # data_dir = "../data"
+    # files = get_midi_files(data_dir)
+    # for file in tqdm(files):
+    #     midi_data = load_file(file)
+
+    metadata_path = "../data/maestro-v3.0.0/maestro-v3.0.0.json"
+    load_metadata(metadata_path)
